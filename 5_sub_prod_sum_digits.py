0cbebdd7089cefaf695de14be960b5f274e205ad
Bathula Venu Gopal <venu99bathula9@gmail.com>
2025-12-19 00:31:50 +0530

    Add files via upload

---
+++ 5_sub_prod_sum_digits.py
@@ -0,0 +1,17 @@
+
+class Solution:
+    def subtractProductAndSum(self, n: int) -> int:
+        product = 1
+        summ = 0
+
+        for d in str(n):
+            digit = int(d)
+            product *= digit
+            summ += digit
+
+        return product - summ
+
+# Example run
+if __name__ == "__main__":
+    sol = Solution()
+    print(sol.subtractProductAndSum(234))
